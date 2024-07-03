97d0bb51063e4c79a15a90cbf0356926e77e5dc6
sanchit mishra <Sanchit.Mishra@contemi.com>
2024-07-03 18:45:47 +0530

    Added the backend APIs

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createTodo, updateTodo } = require('./types')
+const { todo, todo } = require("./db")
 
 const app = express();
 app.use(express.json());
 
 
-app.post("/todos", function(req,res) {
+app.post("/todos", async function(req,res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
 
@@ -15,23 +16,42 @@ app.post("/todos", function(req,res) {
         });
         return;
     }
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed : false
+    })
+
+    res.status(201).json("Todo added successfully");
 })
 
 
-app.get("/todos", function(req,res) {
-    
+app.get("/todos",  async function(req,res) {
+    const todo =  await todo.find();
+    res.status(201).json(todo);
 })
 
 
-app.post("/completed", function(req,res) {
+app.post("/completed",async function(req,res) {
     const completedPayload = req.body;
     const parsedPayload = updateTodo.safeParse(req.body);
 
-    if(!parsedPayload)
+    if(!parsedPayload.success)
         {
             res.status(411).json({
                 msg : "You have sent wrong inputs "
             });
             return;
         }
-        })
+    
+    await todo.update({
+        _id : req.body.id
+    }, {
+        completed : true
+    })
+
+    res.json({
+        msg : "Todo is completed"
+    })
+
+})
